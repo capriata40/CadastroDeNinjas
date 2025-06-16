34a9c40b1fbaba8f3a1487d5cf2ea2c6aedd9b43
Rafael Capriata de Souza Azevedo <rafael.azevedo@mgi.colaboragov.gov.br>
2025-06-16 13:45:03 -0400

    Criado a controller

--- src/main/java/java10x/dev/CadastroDeNinjas/CadastroDeNinjasController.java
+++ src/main/java/java10x/dev/CadastroDeNinjas/CadastroDeNinjasController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class CadastroDeNinjasController {
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
